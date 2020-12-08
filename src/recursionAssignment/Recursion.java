9dd7515cf2c9738197d265e445813fc8fc1bee04
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2020-12-08 22:56:50 +0000

    fixed errors and formatting

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -7,10 +7,12 @@ public class Recursion{
 
     if (intN < 1) {
       return 0;
-    } else if(intN % 10 == 8 && (intN / 10) % 10 == 8){
+    } else if(intN % 10 == 8 && (intN / 10) % 10 == 8) {
       return 2 + count8(intN / 10);
-    } else if(intN % 10 == 8){
+    } else if(intN % 10 == 8) {
       return 1 + count8(intN / 10);
-    } else if()
+    } else {
+      return count8(intN / 10);
+    }
   }
 }
