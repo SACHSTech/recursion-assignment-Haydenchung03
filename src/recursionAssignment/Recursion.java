353976af7996631fd08f809e270a5b4dfc84cdf9
haydenchung03 <hayden.chung21@ycdsbk12.ca>
2020-12-08 21:50:30 +0000

    Failed Test1

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -1,5 +1,9 @@
 package recursionAssignment;
+import java.io.*;
 
 public class Recursion{
 
+  public static int count8(int intn){
+    return intn;
+  }
 }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,34 +1,16 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
 
-import rename-package.Main;
+import recursionAssignment.Recursion;
 
 public class RecursionTest{
-    
-    @Before
-    public void beforeTest(){
-      // some steps to perform before tests are run
-    }
     
     /**
      * An initial test
      */
     @Test
     public void Test1(){
-        // make assertion statement(s)
-        // assertEquals(EXPECTEDVALUE, TESTVALUE);
-        // assertTrue(TESTBOOLEAN);
+      assertEquals(1,Recursion.count8(8));
     }
-    
-    // add more tests
-    
-    @After
-    public void afterTest(){
-      // some steps to perform after tests are run
-    }
-    
-    
 }
